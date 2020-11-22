4e61b6ea78971fc084f475812625194bd38b57c3
FlyingVespa <mailhedri@gmail.com>
2020-11-23 00:11:19 +0100

    commit on Master

--- src/Header.js
+++ src/Header.js
@@ -2,13 +2,13 @@ import React from "react";
 
 import './assets/index.css';
 import './assets/header.css';
-
+import Logo from './assets/img/openbook.png';
 
 const Header =() => {
      return (
      <header>
       <div className="header">
-        <img className="logo-img" src="openbook.png" alt="a"/>
+        <img className="logo-img" src={Logo} alt="a"/>
         <h1>BOOKHUNTER</h1>
       </div>
       <div className="slogan"><p>Bound to find something</p></div>
